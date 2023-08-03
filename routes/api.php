e4a76ec422d51f56de8d6293c63105ba2b4186b9
Alaeldinmusa91 <alaeldinmusa91@gmail.com>
2023-08-04 01:42:29 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,25 +25,31 @@ Route::post('user/register',[AuthController::class,'registerUser']);
 
 Route::post('user/login',[AuthController::class,'login']);
 
-Route::get('user/search',[AuthController::class,'searchUser'])->middleware('auth:sanctum');
-
+Route::get('user/search',[AuthController::class,'searchUser'])
+           ->middleware('auth:sanctum');
 
 /** Route Experience */
 
-Route::post('experience/storejob',[JobTitleController::class,'storeJob'])->middleware('auth:sanctum');
+Route::post('experience/storejob',[JobTitleController::class,'storeJob'])
+            ->middleware('auth:sanctum');
 
-Route::get('experience/getjob',[JobTitleController::class,'index'])->middleware('auth:sanctum');
+Route::get('experience/getjob',[JobTitleController::class,'index'])
+            ->middleware('auth:sanctum');
 
-Route::post('experience/storeindustry',[IndustryController::class,'storeIndustry'])->middleware('auth:sanctum');
+Route::post('experience/storeindustry',[IndustryController::class,'storeIndustry'])
+            ->middleware('auth:sanctum');
 
-Route::get('experience/getindustry',[IndustryController::class,'index'])->middleware('auth:sanctum');
+Route::get('experience/getindustry',[IndustryController::class,'index'])
+           ->middleware('auth:sanctum');
 
-Route::get('experience/getalljob',[JobTitleController::class,'getAllJob'])->middleware('auth:sanctum');
+Route::get('experience/getalljob',[JobTitleController::class,'getAllJob'])
+            ->middleware('auth:sanctum');
 
-Route::post('experience/updateJob/{id}',[JobTitleController::class,'updateJob'])->middleware('auth:sanctum');
+Route::post('experience/updateJob/{id}',[JobTitleController::class,'updateJob'])
+            ->middleware('auth:sanctum');
 
-Route::post('app/apply',[ApplyJobController::class,'applyJob'])->middleware('auth:sanctum');
-
+Route::post('app/apply',[ApplyJobController::class,'applyJob'])
+            ->middleware('auth:sanctum');
 
 /**Route Apply Job */
 
