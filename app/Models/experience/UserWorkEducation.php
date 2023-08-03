593b3a4949249cd5ee8d6743c5cfb750ac92efd5
Alaeldinmusa91 <alaeldinmusa91@gmail.com>
2023-08-04 01:49:02 +0400

    Update UserWorkEducation.php

--- app/Models/experience/UserWorkEducation.php
+++ app/Models/experience/UserWorkEducation.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserWorkEducation extends Model
 {
-    protected $fillable = ['HigestLevel','school','dateCompleted','userId'];
+    protected $fillable = [
+    
+    'HigestLevel'
+    
+    ,'school'
+    
+    ,'dateCompleted'
+    
+    ,'userId'];
 }
